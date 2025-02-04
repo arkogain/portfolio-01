81ca6a536558c89dbc9188ed150c63c9f0c41033
Arko Gain <37438306+arkogain@users.noreply.github.com>
2025-02-04 19:04:10 +0530

    ResumePage>Education & Skill tab done 60%

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -1,5 +1,81 @@
+import EducationTab from "@/components/ResumeComps/EducationTab";
+import SkillsTab from "@/components/ResumeComps/SkillsTab";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
 const page = () => {
-  return <></>;
+  return (
+    <>
+      <section className="min-h-[90dvh] lg:py-20">
+        <Tabs
+          defaultValue="education"
+          className="flex flex-col gap-12 lg:flex-row lg:gap-32"
+        >
+          <TabsList className="flex h-[30dvh] flex-col gap-3 bg-transparent font-jetbrains">
+            {/* <TabsTrigger
+              className="w-64 rounded-lg bg-foreground/5 py-3 text-lg data-[state=active]:bg-default-pink data-[state=active]:text-white"
+              value="exprience"
+            >
+              Expirence
+            </TabsTrigger> */}
+            <TabsTrigger
+              className="w-64 rounded-lg bg-foreground/5 py-3 text-lg data-[state=active]:bg-default-pink data-[state=active]:text-white"
+              value="education"
+            >
+              Education
+            </TabsTrigger>
+
+            <TabsTrigger
+              className="w-64 rounded-lg bg-foreground/5 py-3 text-lg data-[state=active]:bg-default-pink data-[state=active]:text-white"
+              value="skills"
+            >
+              Skills
+            </TabsTrigger>
+
+            <TabsTrigger
+              className="w-64 rounded-lg bg-foreground/5 py-3 text-lg data-[state=active]:bg-default-pink data-[state=active]:text-white"
+              value="about-me"
+            >
+              About Me
+            </TabsTrigger>
+          </TabsList>
+
+          {/* content start */}
+
+          {/* <TabsContent
+            className=""
+            value="exprience"
+          >
+            Exprience
+          </TabsContent> */}
+
+          {/* education Tab */}
+
+          <TabsContent
+            className="text-center font-jetbrains lg:text-start"
+            value="education"
+          >
+            <EducationTab />
+          </TabsContent>
+
+          {/* Skills Tab */}
+
+          <TabsContent
+            className="text-center font-jetbrains lg:text-start"
+            value="skills"
+          >
+            <SkillsTab />
+          </TabsContent>
+
+          <TabsContent
+            className=""
+            value="about-me"
+          >
+            About Me
+          </TabsContent>
+        </Tabs>
+      </section>
+    </>
+  );
 };
 
 export default page;
--- src/lib/information.ts
+++ src/lib/information.ts
@@ -7,26 +7,55 @@ export const navItemsLinks = [
   },
   {
     id: "item02",
-    name: "About",
-    href: "/about",
-    icon: "info-circle",
-  },
-  {
-    id: "item03",
     name: "Resume",
     href: "/resume",
     icon: "file-text",
   },
   {
-    id: "item04",
+    id: "item03",
     name: "Projects",
     href: "/projects",
     icon: "file-text",
   },
   {
-    id: "item05",
+    id: "item04",
     name: "Contact",
     href: "/contact",
     icon: "envelope",
+  },
+];
+
+export const educationDetails = [
+  {
+    id: "item01",
+    boardName: "West Bengal Board of Secondary Education",
+    year: "2014-2015",
+    degree: "10th",
+    marks: "90%",
+    subject: "",
+  },
+  {
+    id: "item02",
+    boardName: "West Bengal Council of Higher Secondary Education",
+    year: "2015-2017",
+    degree: "10+2",
+    marks: "85%",
+    subject: "Pure Science",
+  },
+  {
+    id: "item03",
+    boardName: "West Bengal State University",
+    year: "2017-2020",
+    degree: "Under Graduate",
+    marks: "89%",
+    subject: "Zoology",
+  },
+  {
+    id: "item04",
+    boardName: "Central Institute of Technology",
+    year: "2020-2023",
+    degree: "Fullstack Developer",
+    marks: "90%",
+    subject: "",
   },
 ];
