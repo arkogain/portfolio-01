c36c21a3da9d4a2d5406513b601a02724764bdfb
Arko Gain <37438306+arkogain@users.noreply.github.com>
2025-02-12 18:29:20 +0530

    contact 10% done

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,7 +1,44 @@
+import { Mail, MapPin, PhoneCall } from "lucide-react";
+
 const page = () => {
   return (
     <>
-      <div className=""></div>
+      <section className="grid grid-cols-1 place-items-center lg:grid-cols-2">
+        {/* left Side */}
+        <div className="space-y-5">
+          <div className="flex items-center gap-3">
+            <div className="rounded-lg bg-foreground/10 p-3">
+              <PhoneCall className="text-default-pink" />
+            </div>
+            <div className="flex flex-col">
+              <span>Phone</span>
+              <span>8961042858</span>
+            </div>
+          </div>
+
+          <div className="flex items-center gap-3">
+            <div className="rounded-lg bg-foreground/10 p-3">
+              <Mail className="text-default-pink" />
+            </div>
+            <div className="flex flex-col">
+              <span>Email</span>
+              <span>lukemichel@gmail.com</span>
+            </div>
+          </div>
+
+          <div className="flex items-center gap-3">
+            <div className="rounded-lg bg-foreground/10 p-3">
+              <MapPin className="text-default-pink" />
+            </div>
+            <div className="flex flex-col">
+              <span>Address</span>
+              <span>Barasat</span>
+            </div>
+          </div>
+        </div>
+        {/* left Side */}
+        <div className=""></div>
+      </section>
     </>
   );
 };
